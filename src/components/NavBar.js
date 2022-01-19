e282c7cd70a6af05035de84901dc28f32b0ad8f9
Liza Benitez <leonetkd@gmail.com>
2022-01-19 20:14:19 -0300

    Error npm start en el body

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "./navBar.css";
+
+function NavBar(props) {
+    return (
+        <nav>
+            <ul class="roberto">
+                <li>Home</li>
+                <li>Galeria</li>
+                <li>Info</li>
+                <li>Contactos</li>
+            </ul>
+        </nav>
+    );
+}
+
+
+export default NavBar; 
