ea7f3e218f19ee0d80231074decaf85742d2135c
Liza Benitez <leonetkd@gmail.com>
2022-01-26 12:51:51 -0300

    desafio de navBar e Icono

---
+++ src/components/CardWidget.js
@@ -0,0 +1,10 @@
+import React from "react";
+<script defer src="/your-path-to-fontawesome/js/all.js"></script>
+function CardWidget () {
+    return (
+        <div><i class="fas fa-shopping-cart" alt="logo" width={30} style={{color: "white"}}/> </div>
+        
+        
+    )
+}
+export default CardWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,15 @@
 import "./navBar.css";
+import CardWidget from "./CardWidget";
 
 function NavBar(props) {
     return (
-        <nav>
+        <nav>  
             <ul class="roberto">
                 <li>Home</li>
                 <li>Galeria</li>
                 <li>Info</li>
                 <li>Contactos</li>
+                <li><CardWidget/></li>
             </ul>
         </nav>
     );
